14a8eb0817491964d135d4df48660135f655582e
mimovan <mimovan2210@gmail.com>
2025-06-01 15:25:19 +0700

    Update head

--- resources/views/head.blade.php
+++ resources/views/head.blade.php
@@ -1,4 +1,4 @@
-<meta charset="UTF-8">
+{{-- <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
@@ -72,4 +72,34 @@
     onload="this.onload=null;this.rel='stylesheet'">
 <noscript>
     <link rel="stylesheet" href="{{ asset('template/admin/css/kaiadmin.min.css') }}">
-</noscript>
+</noscript> --}}
+<meta charset="UTF-8">
+<meta name="description" content="Phozogy Template">
+<meta name="keywords" content="Phozogy, unica, creative, html">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>{{ $title }}</title>
+<link rel="icon" type="image/png" href="{{ asset('upload/logo/web.png') }}">
+
+<!-- Google Font -->
+<link href="https://fonts.googleapis.com/css?family=Quantico:400,700&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.5.0/font/bootstrap-icons.min.css">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+
+<!-- Css Styles -->
+<link rel="stylesheet" href="{{ asset('template/css/bootstrap.min.css') }}" type="text/css">
+<link rel="stylesheet" href="{{ asset('template/css/font-awesome.min.css') }}" type="text/css">
+<link rel="stylesheet" href="{{ asset('template/css/elegant-icons.css') }}" type="text/css">
+<link rel="stylesheet" href="{{ asset('template/css/owl.carousel.min.css') }}" type="text/css">
+<link rel="stylesheet" href="{{ asset('template/css/magnific-popup.css') }}" type="text/css">
+<link rel="stylesheet" href="{{ asset('template/css/slicknav.min.css') }}" type="text/css">
+<link rel="stylesheet" href="{{ asset('template/css/style.css') }}" type="text/css">
+<link rel="stylesheet" href="{{ asset('template/css/list-images.css') }}" type="text/css">
+<link rel="stylesheet" href="{{ asset('template/css/reponsive.css') }}" type="text/css">
+
+<!-- CSS Files -->
+{{-- <link rel="stylesheet" href="{{ asset('template/admin/css/plugins.min.css') }}" /> --}}
+<link rel="stylesheet" href="{{ asset('template/admin/css/kaiadmin.min.css') }}" />
+
+<meta name="csrf-token" content="{{ csrf_token() }}">
